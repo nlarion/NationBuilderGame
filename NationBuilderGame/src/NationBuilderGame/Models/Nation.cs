4d4b3685e08a1063414fe9af1849ddf6b1136031
Neil Larion <neil.larion@gmail.com>
2016-05-04 15:37:07 -0700

    Get basic project setup

---
+++ NationBuilderGame/src/NationBuilderGame/Models/Nation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NationBuilderGame.Models
+{
+    [Table("Nations")]
+    public class Nation
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Capital { get; set; }
+        public int Iron { get; set; }
+        public int Timber { get; set; }
+        public int Oil { get; set; }
+        public int Population { get; set; }
+        public float stability { get; set; }
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+        if (!(otherItem is Item))
+        {
+            return false;
+        }
+        else
+        {
+            Item newItem = (Item)otherItem;
+            return this.ItemId.Equals(newItem.ItemId);
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return this.ItemId.GetHashCode();
+    }
+}
---
+++ NationBuilderGame/src/NationBuilderGame/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NationBuilderGame
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<NationBuilderGameContext>(options => options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
